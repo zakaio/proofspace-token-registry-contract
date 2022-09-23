f153234ff5b11029688cfab06d33f0b826732908
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-09-23 16:36:13 +0300

    added nix-shell env

--- smart-contract/trustRegistry/src/Plutus/Contracts/OnChain/TokenCuratedTrustRegistry.hs
+++ smart-contract/trustRegistry/src/Plutus/Contracts/OnChain/TokenCuratedTrustRegistry.hs
@@ -53,6 +53,68 @@ data TokenMoveDatum = TokenMoveDatum {
 
 PlutusTx.makeLift ''TokenMoveDatum
 
+-- We have two types of token: 
+-- control token and issue token
+--
+-- Agents:
+--    Issuers (have did and address)
+--    Voters 
+--         (decide who can be an issuer, holders of owning tokens)
+--    Verifier (proxy for voters)
+--
+-- Control Token
+-- Vote Token
+--  
+--
+-- Issuer    
+--    Can mint issuer tokens, if can complete a vote transaction, where each utxo is a control vote token
+--    
+-- Issuers are represented by address, for conversion between deed and address DidAddress tokens are used.
+
+data VotingPurpose = VotingPurposeMint
+                    |
+                     VotingPurposeBurn
+                    |
+                     ControlVotingPurporeMint
+                    |
+                     ControlVotingPurporeBurn
+
+data VotingRoundParams = VotingRoundParams {
+        name :: BinaryString,
+        redeemCode :: BinaryString,
+        votingPower :: Value
+        finishTime :: PosixTime
+}
+
+
+data VoteTokenParams = VoteTokenParams {
+        purpose :: VotingPurpose,
+        controlToken :: AssetClass,
+        roundParams :: VoteRoundParams
+}
+
+
+mintVoteToken :: VotingTokenParams -> BuiltinData -> BuiltinData -> ()
+mintVoteToken params redeemerData ctxData =
+       if (transactionControlVotingAmount >= paramControlVotingAmount)
+          -- TODO: add check for params in datum
+          ()
+       else
+          error()
+         where 
+            transactionControlVotingAmount = case Map.lookup controlToken txFee of
+                                                Nothing -> 0
+                                                Just tokens -> case Map.lookup controlTokenName tokens of
+                                                   Nothing -> 0
+                                                   Jast amount -> amount
+            paramControlVotingAmount = case Map.lookup controlToken
+            txData = Ledger.txInfoData txInfo
+            txFee = (getValue (Ledger.txInfoFee txInfo))
+            txInfo = Ledger.scriptContextTxInfo ctx
+            ctx = PlutusTx.unsafeFromBuiltinData ctxData
+
+
+
 validateClaim :: TokenMoveDatum -> _ -> ScriptContext -> Bool
 validateClaim tokenMoveDatum _ _ =  False -- TODO: implement
 
